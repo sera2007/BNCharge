aa04f7059ab6ea31b3350cafbc110dab3fc8bdf3
Sergio <sera.serduk@gmail.com>
2018-02-19 14:00:20 +0200

    initial start

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,72 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {Component, ViewChild} from '@angular/core';
+import {AlertController, App, LoadingController, NavController, Slides} from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+	public loginForm: any;
+	public backgroundImage = 'assets/imgs/bg_home.png';
 
-  constructor(public navCtrl: NavController) {
+  constructor(
+	  public loadingCtrl: LoadingController,
+	  public alertCtrl: AlertController,
+	  public app: App
+  ) { }
 
-  }
+  // Slider methods
+  @ViewChild('slider') slider: Slides;
+  @ViewChild('innerSlider') innerSlider: Slides;
+
+	goToStart() {
+		this.slider.slideTo(0);
+	}
+
+  	goToButtons() {
+		this.slider.slideTo(1);
+	}
+
+	goToLogin() {
+		this.slider.slideTo(2);
+	}
+
+	slideNext() {
+		this.innerSlider.slideNext();
+	}
+
+	slidePrevious() {
+		this.innerSlider.slidePrev();
+	}
+
+	presentLoading(message) {
+		const loading = this.loadingCtrl.create({
+			duration: 500
+		});
+
+		loading.onDidDismiss(() => {
+			const alert = this.alertCtrl.create({
+				title: 'Success',
+				subTitle: message,
+				buttons: ['Dismiss']
+			});
+			alert.present();
+		});
+
+		loading.present();
+	}
+
+	login() {
+		this.presentLoading('Thanks for signing up!');
+		// this.navCtrl.push(HomePage);
+	}
+
+	signup() {
+		this.presentLoading('Thanks for signing up!');
+		// this.navCtrl.push(HomePage);
+	}
+	resetPassword() {
+		this.presentLoading('An e-mail was sent with your new password.');
+	}
 
 }
